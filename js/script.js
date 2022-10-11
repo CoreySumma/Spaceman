04b68df2045aa7be97cdf0a1c1d26a816e0d4740
CoreySumma <CoreySumma@gmail.com>
2022-10-11 19:35:27 -0400

    Trying to improve js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
-// Keys = [Q, W, E, R, T, Y, U, I, O, P, 
-//           A, S, D, F, G, H, J, K, L
-//             Z, X, C, V, B, N, M ]
-
-
 /*----- constants -----*/
-const wordBank = ['CRATE','SLANT', 'TRACE', 'ROAST', 'BEANS', 'ADIEU'];
+const wordBank = ['PLANET','ASTEROID', 'UFO', 'COMET', 'DROID', 'GALAXY'];
 
-
-//split word into array once chosen, maybe map()
 
 
 
 /*----- app's state (variables) -----*/
-let userChoice; //letter chosen 
+let userChoice; //Letter chosen/clicked 
 let tries;//How many tries left
-let word;//which word is chosen at random
-let letter;//letter of word 
-let result;// If you guessed the word before tries runs out
+let word;//Represents the word chosen/displayed
+let letter;//Each letter of word 
+let result;// If you guessed the word before tries runs out(6)
 
 
 
@@ -33,19 +26,19 @@ const keysEl = document.querySelectorAll('keys');
 
 /*----- event listeners -----*/
 //EventListener for the buttons -->
+// document.querySelector('#playAgain').addEventListener('click', init);
 document.getElementById('keyboard').addEventListener('click', handleChoice)
 // if the onclick button value === any value of the index of the word chosen 
 //then append that letter to the correlated [idx] of the chosen work into wordEl (renderWord)
 //if it does not match then tries--
 
 //Play again button to re init game
-function handleChoice(evt) {
-    if (evt.target.tagName !== 'BUTTON' || 
+
+function handleChoice(evt) {  //the function for event listener
+    if (evt.target.tagName !== 'BUTTON' || //guards
         userChoice.includes(evt.target.value.toUpperCase())
     ) return;
-    console.log(evt.target.value)
     userChoice.push(evt.target.value.toUpperCase())
-    console.log(userChoice);
     if (word.includes(evt.target.value.toUpperCase())) {
         render();
     } else {
@@ -72,17 +65,19 @@ console.log(word)
 console.log(wordEl)
 function render() {
     renderWord();
+    renderIMG();
+    renderResult();
 }
  
     
-    //display each letter
+   
     // renderTries();//If wrong value selected then tries--
     // renderResult()//If player selects letters of word return winner, else return 'You Lose'
 
 function pickWord() {
     let chosenWord = Math.floor(Math.random() * wordBank.length);//choose from random word index
-    let word = wordBank[chosenWord];//assign word to random word
-    return word.split('');
+    word = wordBank[chosenWord];//assign word to random word
+    return word.split('');//return an array of the word
 }   
 
 function renderWord() {
@@ -94,45 +89,23 @@ function renderWord() {
     })
 }
 
+function renderResult() {
+   if (tries === 0) {
+    document.querySelector('#result').innerHTML = 'You Lose';
+    guessButtonEl.style.visibility = 'hidden';
+   }}
+
+function renderIMG() {
+    const imgPath = `pseudocode/IMG/spaceman-${userChoice.length}.png`;
+    console.log(imgPath);
+    let spacemanEl = document.querySelector('#spacemanIMG');
+    spacemanEl.style.backgroundImage = `url("${imgPath}")`;
+}
 
 
 
-// function renderChoice() {
-// //     if (keysEl === choice of key value) {
-// // insert letter on the idx of _
-// //or switch it?
-//     }
 
-
-// function renderTries() {
-// if (letter !== word[idx]) {
-//     return tries --
-// }
-// return
-// }
-
-// function renderResult() {
-// if (tries < 0 && guess === word.length) {  //define guess/winner
-//     return winner;
-// } 
-
-// }
-
-// }
-
-
-
-// General Flow
-// Start game button will randomly choose a word
-
-// Loop through length of the word and add a border for each letter to the 'word' div
-//renderWord
-
-// on click letter(s) will loop through the words letters and determine:
-
-                    // If the player clicks the wrong letter then the 
-                    //letter will no longer be clickable
-                    // and the air count will go down by -1
-
-                    // Otherwise(picks correct letter): the letter will be pushed 
-                    // onto it's index correlated div
+// for (let i = 0; i < userChoice.length; i++)
+// letter = userChoice[i];
+// if (userChoice[i].length > tries) {
+// document.querySelector('#result').innerHTML = 'He is dead, try again';
